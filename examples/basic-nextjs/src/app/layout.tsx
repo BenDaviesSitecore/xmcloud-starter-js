03c164d2a664f3300c4eb19bfb5fb9e0783db231
Ben Davies <171953419+BenDaviesSitecore@users.noreply.github.com>
2026-08-13 09:48:56 +0100

    Updates

--- examples/basic-nextjs/src/app/layout.tsx
+++ examples/basic-nextjs/src/app/layout.tsx
@@ -1,10 +1,32 @@
-import './globals.css';
+import "../assets/main.scss";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://edge-platform.sitecorecloud.io" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>{children}</body>
